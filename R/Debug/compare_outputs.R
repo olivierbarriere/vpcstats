d560ad4fca8e5871c537ba1c38cf6c86ac2c0d03
Olivier Barriere <olivier.barriere@gmail.com>
2018-09-06 14:47:24 -0400

    Updated output

--- R/Debug/compare_outputs.R
+++ R/Debug/compare_outputs.R
@@ -1,22 +1,26 @@
 compare_outputs = function(df1, df2) {
-  arrangevars = names(df2)[1:which(names(df2)=="DVPI")]
-
+  arrangevars = names(df2)[1:which(names(df2)=="DVQNAME")]
+  
   df1 = df1 %>%
     ungroup() %>%
     rename(BIN=BINS,
-           DVPI=quantilename,
+           DVQNAME=quantilename,
            `DV2.5%CI`=QELCI,
            `DV50%CI`=QEMCI,
            `DV97.5%CI`=QEUCI,
            DVOBS=quantilevalue) %>%
     arrange_at(arrangevars)
   
-  if ("percentblqQELCI" %in% names(df1)) df1 = df1 %>% rename(`PCTBLQ2.5%CI`=percentblqQELCI,
-                                                              `PCTBLQ50%CI`=percentblqQEMCI,
-                                                              `PCTBLQ97.5%CI`=percentblqQEUCI, 
-                                                              PCTBLQOBS=percentblq)
+  df1$DVQNAME=ifelse(df1$DVQNAME=="PLPI", "5%PI", ifelse(df1$DVQNAME=="PMPI", "50%PI", ifelse(df1$DVQNAME=="PUPI", "95%PI", NA)))
   
-  df1$DVPI=ifelse(df1$DVPI=="PLPI", "5%", ifelse(df1$DVPI=="PMPI", "50%", ifelse(df1$DVPI=="PUPI", "95%", NA)))
+  if ("blqquantilename" %in% names(df1)) {
+    df1 = df1 %>% rename(PCTBLQQNAME=blqquantilename,
+                         `PCTBLQ2.5%CI`=percentblqQELCI,
+                         `PCTBLQ50%CI`=percentblqQEMCI,
+                         `PCTBLQ97.5%CI`=percentblqQEUCI, 
+                         PCTBLQOBS=percentblq)
+    df1$PCTBLQQNAME=ifelse(df1$PCTBLQQNAME=="percentblq", "PercentBLQ", NA)
+  }  
   
   df2 = df2 %>%
     arrange_at(arrangevars)
